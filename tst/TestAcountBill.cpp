c0b405c44b401cd1ba98a71f818d777b28575ac5
Eric Zhang <zhang.lyuan@gmail.com>
2016-07-21 21:06:52 +0800

    make the compilation pass

--- tst/TestAcountBill.cpp
+++ tst/TestAcountBill.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include "Account.hpp"
+
 class TestAccount : public ::testing::Test{
 
 };
@@ -7,7 +9,7 @@ class TestAccount : public ::testing::Test{
 TEST(TestAccount, GivenGoldAcountWithOneLineShallHaveMainLineCost)
 {
   Account account = Account();
-  double bill = account.Bill(GOLD, 1);
+  double bill = account.Bill(Gold, 1);
   
   EXPECT_EQ(49.95, bill);
 }
